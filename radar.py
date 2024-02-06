6ab2dbeb7934c184b8f2df44d7a56977e450fefe
Nathan Lo <natefung2012@gmail.com>
2024-02-06 12:17:14 -0500

    chore: progress sync

--- radar.py
+++ radar.py
@@ -3,14 +3,6 @@ import numpy as np
 
 
 class Radar:
-    # def __init__(self, xy: list[list[int]], arena_width: list[int], arena_height: list[int]):
-    #     x = [i[0] for i in xy]
-    #     y = [i[1] for i in xy]
-
-    #     self.arena_image_coords = np.array([[x[0], y[0]], [x[1], y[1]], [x[2], y[2]], [x[3], y[3]]], dtype=np.float32)
-    #     self.arena_coords = np.array([[0, 0], [arena_width - 1, 0], [arena_width - 1, arena_height - 1], [0, arena_height - 1]], dtype=np.float32)
-    #     self.homography_matrix, _ = cv2.findHomography(self.arena_image_coords, self.arena_coords)
-
     def __init__(
         self, xy: list[list[int]], ref_xy: list[list[int]]
     ):
--- ui.py
+++ ui.py
@@ -1,9 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+
+class UI:
+
+    def __init__(self, arena_map, camera_feed):
+        self.root = Tk()
+        self.frm = ttk.Frame(radar, padding=10)
+        self.frm.grid()
+        self.ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+        self.ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=0)
+        self.ttk.Label(frm, text="Yeehaw").grid(column=1, row=0)
+        self.frm.update_idletasks()
+        self.frm.update()
